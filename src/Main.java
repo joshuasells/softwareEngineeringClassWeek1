1cd294674c5f3e4322c561431ca54932fbe1a017
Joshua Sells <joshuataylorsells@gmail.com>
2021-05-17 12:17:20 -0500

    implemens the adding of pets. Ready fore release 1

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Strart off of a loop for the main program
-        while (true) {
-          // Create new scanner object
-          Scanner input = new Scanner(System.in);
+      // new PetCollection object
+      PetCollection pets = new PetCollection();
+      
+      // loop for the main program
+      while (true) {
+        // Create new scanner object
+        Scanner input = new Scanner(System.in);
+        
+        printMenu();
+        
+        String choice = input.nextLine();
 
-          PetCollection pets = new PetCollection();
-          pets.addPet(new Pet("Simon", 1));
-          pets.displayPets();
-          break;
+        switch (choice) {
+          // Display all the pets
+          case "1" :
+            pets.displayPets();
+            break;
+
+          // Add some pets
+          case "2" :
+            // set counter for tracking how many pets we add
+            int counter = 0;
+            // loop to add pets until user enters done
+            while (true) {
+              System.out.print("add pet (name, age): ");
+              String petDetails = input.nextLine();
+              if (petDetails.equalsIgnoreCase("done")) {
+                break;
+              }
+              String[] newPet = petDetails.split(" ");
+              pets.addPet(new Pet(newPet[0], Integer.parseInt(newPet[1])));
+              counter++;
+            }
+            System.out.println(counter + " pets added.");
+            break;
+          case "3" :
+            System.exit(1);
+          default :
+            System.out.println("Please enter a valid number");
+            break;
         }
+      }
     }
 
     public static void printMenu() {
-      System.out.println("yes sir");
+      System.out.print("Pet Database Program\n\n");
+      System.out.println("What would you like to do?");
+      System.out.println("1) View all pets");
+      System.out.println("2) Add more pets");
+      System.out.println("3) Exit Program");
+      System.out.print("Your choice: ");
     }
 }
--- src/PetCollection.java
+++ src/PetCollection.java
@@ -8,22 +8,26 @@ import java.util.ArrayList;
 
 public class PetCollection {
 
-  private ArrayList<Pet> pets = new ArrayList<>();
+  public ArrayList<Pet> pets = new ArrayList<>();
 
   public void addPet(Pet pet) {
     pets.add(pet);
   }
 
   public void displayPets() {
-    System.out.printf("+----------------------+\n");
+    // Prints the header
+    System.out.printf("\n+----------------------+\n");
     System.out.printf("| ID | %-9s | AGE |\n", "NAME");
     System.out.printf("+----------------------+\n");
     
+    // Prints a line for each Pet object in the PetCollection object
+    // Prints the id, name, and age of the pet
     for (int i = 0; i < pets.size(); i++) {
       System.out.printf("| %2d | %-9s | %3d |\n", i, pets.get(i).getName(), pets.get(i).getAge());
     }
 
+    // Prints footer
     System.out.printf("+----------------------+\n");
-    System.out.printf("%d rows in set\n", pets.size());
+    System.out.printf("%d rows in set\n\n", pets.size());
   }
 }
